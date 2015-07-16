407fea2896492479d6add8763654f14d3feadc56
Anton Szorad <anton.szorad@gmail.com>
2015-07-16 21:46:23 +0200

    2nd commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
@@ -10,6 +6,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Just testing git hub");
         }
     }
 }
