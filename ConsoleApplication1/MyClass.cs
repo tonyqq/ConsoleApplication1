036b9cdcee591db93b6aff337cf6038004bd80a3
Szórád Anton (ERNI) <Anton.Szorad@erni.sk>
2015-07-23 21:22:52 +0200

    Added MyClass into source control

---
+++ ConsoleApplication1/MyClass.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApplication1
+{
+    using System;
+
+    public class MyClass
+    {
+        public void MyMethod()
+        {
+        }
+
+        public void MyMethod(int n)
+        {
+        }
+        public void MyMethod2()
+        {
+        }
+    }
+}
